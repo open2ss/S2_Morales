ef38bd23ae6990dc785177d2441d3de4d39e026c
Aaron morales <aaron_15_itse@hotmail.com>
2020-10-30 04:23:11 -0500

    NUEVA VARIABLE

---
+++ S2_Morales/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace S2_Morales
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValidar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double parcial1, parcial2, notaFinal;
+                double n1 = Convert.ToDouble(txtN1.Text);
+                double e1 = Convert.ToDouble(txtE1.Text);
+
+
+                double n2 = Convert.ToDouble(txtN2.Text);
+                double e2 = Convert.ToDouble(txtE2.Text);
+
+
+                parcial1 = (n1 * 0.3) + (e1 * 0.2);
+                parcial2 = (n2 * 0.3) + (e2 * 0.2);
+                notaFinal = parcial1 + parcial2;
+
+                txtT1.Text = parcial1.ToString();
+                txtT2.Text = parcial2.ToString();
+                txtNF.Text = notaFinal.ToString();
+
+                if (notaFinal >= 7)
+                {
+                    DisplayAlert("Aprobado", "Su calificacion es de: " + notaFinal + "/10", "Aceptar");
+                    txtNF.BackgroundColor = Color.Green;
+                }
+                else
+                {
+                    DisplayAlert("Reprobado", "Su calificacion es de: " + notaFinal + "/10", "Aceptar");
+                    txtNF.BackgroundColor = Color.Red;
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Alerta", ex.Message, "Aceptar");
+            }
+        }
+    }
+}
+
---
+++ S2_Morales/login.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace S2_Morales
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class login : ContentPage
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnMainPage_Clicked1(object sender, EventArgs e)
+        {
+            if (txtUser.Text.Equals("estudiante2020") && txtPassword.Text.Equals("uisrael2020"))
+            {
+                await Navigation.PushAsync(new MainPage());
+
+            }
+            else
+            {
+                DisplayAlert("Mensaje de alerta", "clave incorrecta", "vuelva intentarlo");
+            }
+        }
+    }
+}
